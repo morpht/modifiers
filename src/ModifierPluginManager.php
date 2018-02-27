e9ae72d57286a9855769449ebec0128c5bcac7c1
Vit Hovezak <kybersoft@gmail.com>
2018-02-27 10:21:34 +0100

    Issue #2934388 by kyberman: Discover Modifiers defined in theme

--- src/ModifierPluginManager.php
+++ src/ModifierPluginManager.php
@@ -4,7 +4,11 @@ namespace Drupal\modifiers;
 
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Extension\ThemeHandlerInterface;
 use Drupal\Core\Plugin\DefaultPluginManager;
+use Drupal\Core\Plugin\Discovery\AnnotatedClassDiscovery;
+use Drupal\Core\Plugin\Discovery\ContainerDerivativeDiscoveryDecorator;
+use Drupal\Core\Plugin\Discovery\YamlDiscoveryDecorator;
 
 /**
  * Provides a Modifier plugin manager.
@@ -17,6 +21,13 @@ use Drupal\Core\Plugin\DefaultPluginManager;
 class ModifierPluginManager extends DefaultPluginManager {
 
   /**
+   * The theme handler.
+   *
+   * @var \Drupal\Core\Extension\ThemeHandlerInterface
+   */
+  protected $themeHandler;
+
+  /**
    * Constructs a ModifierPluginManager object.
    *
    * @param \Traversable $namespaces
@@ -26,12 +37,34 @@ class ModifierPluginManager extends DefaultPluginManager {
    *   Cache backend instance to use.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
    *   The module handler.
+   * @param \Drupal\Core\Extension\ThemeHandlerInterface $theme_handler
+   *   The theme handler.
    */
-  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
+  public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler, ThemeHandlerInterface $theme_handler) {
     parent::__construct('Plugin/modifiers', $namespaces, $module_handler, 'Drupal\modifiers\ModifierInterface', 'Drupal\modifiers\Annotation\Modifier');
 
+    $this->themeHandler = $theme_handler;
     $this->setCacheBackend($cache_backend, 'modifiers_plugins');
     $this->alterInfo('modifiers_info');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDiscovery() {
+    if (!$this->discovery) {
+      $discovery = new AnnotatedClassDiscovery($this->subdir, $this->namespaces, $this->pluginDefinitionAnnotationName, $this->additionalAnnotationNamespaces);
+      $discovery = new YamlDiscoveryDecorator($discovery, 'modifiers', $this->moduleHandler->getModuleDirectories() + $this->themeHandler->getThemeDirectories());
+      $this->discovery = new ContainerDerivativeDiscoveryDecorator($discovery);
+    }
+    return $this->discovery;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function providerExists($provider) {
+    return $this->moduleHandler->moduleExists($provider) || $this->themeHandler->themeExists($provider);
   }
 
 }
